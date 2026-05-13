e28304e23767b02ee4e544826d9810671d86e86a
akarsh-sidhartha <akarsh014@gmail.com>
2026-05-13 08:34:33 +0530

    Cleanup: Remove UI folders for dedicated backend repo

--- public-site/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomePage from './pages/HomePage';
-import './App.css';
-
-function App() {
-  return <HomePage />;
-}
-
-export default App;
--- run_app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend Server...
-start cmd /k "cd /d E:\Marqland_Studios\marqland-catalogue\backend && node server.js"
-
-echo Starting Frontend React App...
-start cmd /k "cd /d E:\Marqland_Studios\marqland-catalogue\frontend && npm start"
-
-echo Application processes launched.
